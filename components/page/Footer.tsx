aaa1611b3312191314257bfc662cff0b0d1fcbc5
Kristopher Rahim <kristopherab@gmail.com>
2023-07-28 22:37:14 +0100

    add links to footer

--- components/page/Footer.tsx
+++ components/page/Footer.tsx
@@ -2,4 +2,16 @@
 /** @jsxFrag Fragment */
 import { jsx, memo } from "deps";
 
-export const Footer = memo(() => <footer>Powered by Hono</footer>);
+export const Footer = memo(() => (
+    <footer>
+        <small>Powered by Hono</small>
+        <ul>
+            <li>
+                <a href="https://github.com/adoublef-js/hateoas">Github</a>
+            </li>
+            <li>
+                <a href="https://hateoas.adoublef.dev">Home</a>
+            </li>
+        </ul>
+    </footer>
+));
