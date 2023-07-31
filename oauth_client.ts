353881ec1fbb6d850c6828bb824797e3af5ac31d
Kristopher Rahim <kristopherab@gmail.com>
2023-07-31 20:50:42 +0100

    move oauth client to own file

---
+++ oauth_client.ts
@@ -0,0 +1,27 @@
+import { createAuth0OAuth2Client, createRemoteJWKSet } from "deps";
+
+const oauthClient = createAuth0OAuth2Client({
+    redirectUri: `${Deno.env.get("APP_URL")}/i/callback`,
+    defaults: {
+        scope: Deno.env.get("AUTH0_SCOPE")!.split(","),
+    },
+});
+
+const oauthDomain = new URL(oauthClient.config.authorizationEndpointUri).origin;
+
+const jwksUrl = new URL(`${oauthDomain}/.well-known/jwks.json`);
+const jwks = createRemoteJWKSet(jwksUrl);
+
+const params = new URLSearchParams();
+params.append("returnTo", new URL(oauthClient.config.redirectUri!).origin);
+params.append("client_id", oauthClient.config.clientId);
+
+const logoutUrl = new URL(`v2/logout?${params}`, oauthDomain);
+const audience = Deno.env.get("AUTH0_AUDIENCE")?.split(",");
+
+export const oauthConfig = {
+    client: oauthClient,
+    jwks,
+    aud: audience,
+    logoutUrl,
+} as const;
